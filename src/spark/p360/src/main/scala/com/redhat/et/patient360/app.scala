befa374ec4fbf36038a6786201a65bb0302ed56e
Developer <developer@me.com>
2018-02-17 15:11:50 +0100

    Added Spark source code

---
+++ src/spark/p360/src/main/scala/com/redhat/et/patient360/app.scala
@@ -0,0 +1,16 @@
+package com.redhat.et.patient360;
+
+import com.redhat.et.silex.app.AppCommon
+
+object QueryApp extends AppCommon {
+  override def appName = "patient360-demo"
+  
+  // the args to main should be the JDBC url and the table name
+  override def appMain(args: Array[String]) {
+    require(args.length == 4, "usage:  QueryApp USERNAME PASSWORD JDBC_URL TABLE_NAME")
+    val source = sqlContext.read.format("jdbc").options(Map("user" -> args(0), "password" -> args(1),"url" -> args(2),"dbtable" -> args(3))).load()
+    val results = Queries.bpWarnings(source)
+    results.show(results.count.toInt, false)
+    results.write.format("com.databricks.spark.csv").save("/Users/cvanball/summit/p360_output/alerts.csv")
+  }
+}
