f85087583fda4ce930d51b15c8f895903f6d71bf
Madou <mcouliba@redhat.com>
2018-02-26 17:51:47 +0100

    Upgraded to run with sbt 1.1.1

--- src/spark/runsparkjob.sh
+++ src/spark/runsparkjob.sh
@@ -3,7 +3,7 @@ OUTPUT_DIR=$1
 
 rm -rf ${OUTPUT_DIR}/alerts.csv
 cd p360
-sbt  "run teiidUser redhat1! jdbc:teiid:RTDS@mm://localhost:31000 PATIENT360.PATIENT" -DSPARK_OUTPUT_DIR=${OUTPUT_DIR}
+sbt -Dspark.output.dir=${OUTPUT_DIR} "run teiidUser redhat1! jdbc:teiid:RTDS@mm://localhost:31000 PATIENT360.PATIENT" 
 
 cd ${OUTPUT_DIR}/..
 find . -name "part*" -exec sh -c 'mv "$1" "${1%.gappedPeak}.csv"' _ {} \;
